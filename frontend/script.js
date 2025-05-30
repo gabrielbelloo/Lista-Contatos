5cf9204d8e6bd21ed8eab8d16ca92d91f2b29da2
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-05-30 11:21:54 -0300

    Created Vcards

--- frontend/script.js
+++ frontend/script.js
@@ -5,21 +5,21 @@ function filterTable() {
     let emailInput = document.getElementById("search-email").value.toLowerCase();
     let phoneInput = document.getElementById("search-phone").value.toLowerCase();
     let rows = document.querySelectorAll("#table-body tr");
-    
+
     rows.forEach(row => {
         let name = row.children[0].textContent.toLowerCase();
         let sector = row.children[1].textContent.toLowerCase();
         let ramal = row.children[2].textContent.toLowerCase();
         let email = row.children[3].textContent.toLowerCase();
         let phone = row.children[4].textContent.toLowerCase();
-        
-        let matches = 
+
+        let matches =
             (name.includes(nameInput) || nameInput === "") &&
             (sectorInput === "" || sector.includes(sectorInput)) &&
             (ramal.includes(ramalInput) || ramalInput === "") &&
             (email.includes(emailInput) || emailInput === "") &&
             (phone.includes(phoneInput) || phoneInput === "");
-        
+
         row.style.display = matches ? "" : "none";
     });
 }
@@ -28,14 +28,14 @@ function sortTable(columnIndex) {
     let table = document.querySelector("table tbody");
     let rows = Array.from(table.rows);
     let ascending = table.dataset.order !== "asc";
-    
+
     rows.sort((a, b) => {
         let cellA = a.cells[columnIndex].textContent.trim().toLowerCase();
         let cellB = b.cells[columnIndex].textContent.trim().toLowerCase();
-        
+
         return ascending ? cellA.localeCompare(cellB) : cellB.localeCompare(cellA);
     });
-    
+
     table.innerHTML = "";
     rows.forEach(row => table.appendChild(row));
     table.dataset.order = ascending ? "asc" : "desc";
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 async function fetchContatos() {
     try {
         const response = await fetch('https://contatos.plconfeccoes.com.br/api/contatos');
-        
+
         if (!response.ok) {
             throw new Error('Erro ao buscar contatos');
         }
@@ -73,6 +73,7 @@ async function fetchContatos() {
                 <td>${contato.email} <a href="mailto:${contato.email}"><i class="fa-solid fa-paper-plane"></i></a></td>
             `;
             tableBody.appendChild(row);
+            row.addEventListener('click', () => openContactCard(contato));
         });
     } catch (err) {
         console.error('Erro ao buscar contatos:', err);
@@ -244,6 +245,25 @@ function closeAddContactModal() {
     document.getElementById('addContactModal').style.display = 'none';
 }
 
+function openContactCard(contato) {
+    document.getElementById('contactCard').style.display = 'flex';
+    document.getElementById('contactName').textContent = contato.nome;
+    document.getElementById('contactSector').textContent = contato.setor;
+
+    document.getElementById('contactRamal').innerHTML =`<i class="fa-solid fa-phone"></i> <a>${contato.ramal}</a>`;
+    document.getElementById('contactPhone').innerHTML =`<i class="fa-brands fa-whatsapp"></i> <a target="blank" href="https://wa.me/55${formatPhoneNumber(contato.telefone)}">${contato.telefone}</a>`;
+    document.getElementById('contactEmail').innerHTML =`<i class="fa-solid fa-envelope"></i> <a target="blank" href="mailto:${contato.email}">${contato.email}</a>`;
+}
+
+function closeContactCard() {
+    document.getElementById('contactCard').style.display = 'none';
+    document.getElementById('contactName').textContent = '';
+    document.getElementById('contactSector').textContent = '';
+    document.getElementById('contactRamal').innerHTML = '';
+    document.getElementById('contactPhone').innerHTML = '';
+    document.getElementById('contactEmail').innerHTML = '';
+}
+
 const adminLogin = "admin";
 const adminPassword = "admin123";
 
@@ -252,7 +272,7 @@ function validateLogin() {
     const password = document.getElementById('password').value;
 
     if (login === adminLogin && password === adminPassword) {
-        closeLoginModal(); 
+        closeLoginModal();
         document.querySelectorAll('button').forEach(button => button.classList.remove('hidden'));
     } else {
         document.getElementById('errorMessage').textContent = 'Login ou senha incorretos.';
