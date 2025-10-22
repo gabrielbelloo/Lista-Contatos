e4033b48e13f9ceedc82193202b86c92f5bd77e0
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-10-21 22:24:26 -0300

    Imported React

---
+++ contacts-list/src/App.jsx
@@ -0,0 +1,14 @@
+import Contacts from './components/Contacts.jsx';
+import Filter from './components/Filter.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>Contacts List</h1>
+      <Contacts />
+      <Filter />
+    </div>
+  );
+}
+
+export default App;
