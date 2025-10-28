084e95b67a7fd9a95e9a7e6af0856a5e50b52942
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2025-10-28 14:31:37 -0300

    Feat (Header)
    Add Header component

--- contacts-list/src/App.jsx
+++ contacts-list/src/App.jsx
@@ -1,11 +1,12 @@
 import Contacts from "./components/Contacts.jsx";
 import Filter from "./components/Filter.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <div className="isolate w-full bg-slate-800">
-      <Sidebar />
+    <div className="isolate h-full w-full bg-slate-800">
+      <Header />
     </div>
   );
 }
