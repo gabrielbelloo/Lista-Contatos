38b51098ba586ec176d473990c12093d992862f6
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-04-24 15:52:01 -0300

    feat(ContactMapper): add updateEntity

--- server/src/main/java/com/bello/contact/mappers/ContactMapper.java
+++ server/src/main/java/com/bello/contact/mappers/ContactMapper.java
@@ -1,9 +1,7 @@
 package com.bello.contact.mappers;
 
 import com.bello.contact.dtos.ContactDTO;
-import com.bello.contact.entities.ContactEntity;
-import com.bello.contact.entities.DepartmentEntity;
-import com.bello.contact.entities.ExtensionEntity;
+import com.bello.contact.entities.*;
 
 public class ContactMapper {
 
@@ -18,7 +16,11 @@ public class ContactMapper {
         if (dto.getPhones() != null) {
             entity.setPhones(
                     dto.getPhones().stream()
-                            .map(PhoneMapper::toEntity)
+                            .map(p -> {
+                                PhoneEntity phone = PhoneMapper.toEntity(p);
+                                phone.setContact(entity);
+                                return phone;
+                            })
                             .toList()
             );
         }
@@ -26,7 +28,11 @@ public class ContactMapper {
         if (dto.getEmails() != null) {
             entity.setEmails(
                     dto.getEmails().stream()
-                            .map(EmailMapper::toEntity)
+                            .map(e -> {
+                                EmailEntity email = EmailMapper.toEntity(e);
+                                email.setContact(entity);
+                                return email;
+                            })
                             .toList()
             );
         }
@@ -49,6 +55,43 @@ public class ContactMapper {
             dto.setExtensionId(entity.getExtension().getId());
         }
 
+        if (entity.getPhones() != null){
+            dto.setPhones(
+                    entity.getPhones().stream()
+                            .map(PhoneMapper::toDTO)
+                            .toList()
+            );
+        }
+
+        if (entity.getEmails() != null){
+            dto.setEmails(
+                    entity.getEmails().stream()
+                            .map(EmailMapper::toDTO)
+                            .toList()
+            );
+        }
+
         return dto;
+    }
+
+    public static void updateEntity(ContactEntity entity, ContactDTO dto, DepartmentEntity department, ExtensionEntity extension){
+        entity.setName(dto.getName());
+        entity.setPosition(dto.getPosition());
+        entity.setDepartment(department);
+        entity.setExtension(extension);
+
+        entity.getPhones().clear();
+        dto.getPhones().forEach(p -> {
+            PhoneEntity phone = PhoneMapper.toEntity(p);
+            phone.setContact(entity);
+            entity.getPhones().add(phone);
+        });
+
+        entity.getEmails().clear();
+        dto.getEmails().forEach(e -> {
+            EmailEntity email = EmailMapper.toEntity(e);
+            email.setContact(entity);
+            entity.getEmails().add(email);
+        });
     }
 }
